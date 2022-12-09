c84ec00317651d5ac08e137e553141124f20bb55
Matěj Kmínek <mkminek11@seznam.cz>
2022-12-09 16:48:39 +0100

    left side bar

--- lib/play.py
+++ lib/play.py
@@ -2,53 +2,16 @@ import pyglet
 from typing import Union
 from pathlib import Path
 
-class List:
-    def __init__(self, window:pyglet.window.Window, items:list=[], item_width:int=64, padding:int=5, place:str="top", color:tuple[int, int, int]=(200, 200, 200)):
-        self.bg = pyglet.shapes.Rectangle(0, 0, 0, 0, color)
-        self.items = items
-        self.item_width = item_width
-        self.padding = padding
-        self.place = place
-        self.window = window
-    
-    def draw(self):
-        self.bg.draw()
-        for i in self.buttons:
-            i.draw()
 
-    def _update_buttons(self, w, h):
-        in_row = (w) // (self.item_width+self.padding)
-        buttons = ["road", "quit", "map "]    # THIS MAY RAISE AN ERROR - just add new command
-        match self.place:
-            case "top":
-                self.bg.width = w
-                self.bg.height = (len(self.items) // in_row + 1) * (self.item_width+self.padding) + self.padding
-                self.bg.x = 0
-                self.bg.y = self.window.height - self.bg.height
-        self.buttons = [Button(
-            image = x[0],
-            x = self.padding+(n%in_row)*(self.item_width+self.padding), 
-            y = h - self.item_width - (self.padding+(n//in_row)*(self.item_width+self.padding)), 
-            width = self.item_width, 
-            action = buttons[n],
-            image_mouse_on = x[1]) for n, x in enumerate(self.items)]
-
-    def update(self, w, h):
-        self.bg.y = h - self.bg.height
-        self.bg.width = w
-        self._update_buttons(w, h)
-
-def spritesheet(image:Union[str, Path], width:int=1, height:int=1):
-    a = list(pyglet.image.ImageGrid(pyglet.image.load(image), height, width))
-    for row in range(0, len(a)//height):
-        yield [a[row+x*width] for x in range(len(a)//width)][::-1]
 
 class Button:
     def __init__(self, image, x, y, width, action, image_mouse_on=None, image_pressed=None):
         self.sprite = pyglet.sprite.Sprite(image, x, y)
-        self.sprite.scale = width / self.sprite.width
+        # self.sprite.scale = width / self.sprite.width
         self.forms = [image, image_mouse_on, image_pressed]
         self.action = action
+        self.x = x
+        self.y = y
 
     def check_mouse_over(self, x:int=0, y:int=0, pressed:bool=False):
         if x > self.sprite.x and x < self.sprite.x + self.sprite.width and y > self.sprite.y and y < self.sprite.y + self.sprite.height:
@@ -61,4 +24,81 @@ class Button:
                 self.sprite.image = self.forms[0]
 
     def draw(self):
+        self.sprite.x = self.x
+        self.sprite.y = self.y
         self.sprite.draw()
+
+
+
+
+class TopList:
+    def __init__(self, window:pyglet.window.Window, items:list[Button]=[], item_width:int=64, padding:int=5, place:str="top", color:tuple[int, int, int]=(200, 200, 200)):
+        self.bg = pyglet.shapes.Rectangle(0, 0, 0, 0, color)
+        self.buttons = items
+        self.item_width = item_width
+        self.padding = padding
+        self.place = place
+        self.window = window
+        self.update(window.width, window.height)
+        buttons = ["road", "map ", "quit"]    # THIS MAY RAISE AN ERROR - just add new command
+        for n, i in enumerate(self.buttons):
+            i.sprite.scale = 0.5
+            i.action = buttons[n]
+    
+    def draw(self):
+        self.bg.draw()
+        for i in self.buttons:
+            i.draw()
+
+    def _update_buttons(self, w, h):
+        in_row = (w) // (self.item_width+self.padding)
+        match self.place:
+            case "top":
+                self.bg.width = w
+                self.bg.height = (len(self.buttons) // in_row + 1) * (self.item_width+self.padding) + self.padding
+                self.bg.x = 0
+                self.bg.y = self.window.height - self.bg.height
+
+        for n, b in enumerate(self.buttons): 
+            b.x = self.padding+(n%in_row)*(self.item_width+self.padding)
+            b.y = h - self.item_width - (self.padding+(n//in_row)*(self.item_width+self.padding))
+
+    def update(self, w, h):
+        self.bg.y = h - self.bg.height
+        self.bg.width = w
+        self._update_buttons(w, h)
+
+
+
+class SideList:
+    def __init__(self, window:pyglet.window.Window, buttons:list[Button]=[], item_height:int=64, padding:int=2, color:tuple[int, int, int]=(100, 100, 100), opacity:int=50):
+        self.buttons = buttons
+        self.item_height = item_height
+        self.padding = padding
+        _height = len(buttons) * (item_height + padding) + padding
+        self.bg = pyglet.shapes.Rectangle(0, window.height//2-_height//2, item_height+2*padding, _height, color)
+        self.bg.opacity = opacity
+        self.update(window.height)
+        actions = ["++++", "----", "base"]
+        for n, i in enumerate(self.buttons):
+            i.action = actions[n]
+
+    def update(self, h):
+        self.bg.y = h//2 - self.bg.height//2
+        for n, i in enumerate(self.buttons):
+            i.x = self.padding
+            i.y = self.bg.y + n*(self.padding+self.item_height)
+
+    def draw(self):
+        self.bg.draw()
+        for i in self.buttons:
+            i.draw()
+
+
+
+def spritesheet(image:Union[str, Path], width:int=1, height:int=1):
+    a = list(pyglet.image.ImageGrid(pyglet.image.load(image), height, width))
+    for col in range(0, len(a)//height):
+        yield Button(a[width+col], 0, 0, 0, "noe", image_mouse_on=a[col])
+
+# print(list(spritesheet("S:\\Matysek\\_PROGRAMOVÁNÍ\\Python\\_hry\\_Transporter\\img\\buttons1.png", 3, 2)))
--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from lib.transport import *
 
 w = pyglet.window.Window(resizable=True)
 
-buttons_list = list(spritesheet("img/buttons.png", 4, 2))
-l = List(w, [buttons_list[0], buttons_list[3], buttons_list[1]])
+buttons1_list = list(spritesheet("img/buttons1.png", 4, 2))
+buttons2_list = list(spritesheet("img/buttons2.png", 3, 2))
+l = TopList(w, [buttons1_list[0], buttons1_list[3], buttons1_list[1]])
+s = SideList(w, buttons2_list)
 g = Grid(100, 100)
 
 # c = Car1("car1.png", 10)
@@ -16,11 +18,8 @@ def on_draw():
     w.clear()
     g.draw(w.width, w.height-l.bg.height)
     l.draw()
+    s.draw()
     # c.draw()
-
-@w.event
-def on_mouse_scroll(x, y, scroll_x, scroll_y):
-    g.set_scale(g.scale + 0.5 * scroll_y, x, y)
 
 @w.event
 def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
@@ -29,6 +28,8 @@ def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
 @w.event
 def on_mouse_motion(x, y, dx, dy):
     for b in l.buttons:
+        b.check_mouse_over(x, y)
+    for b in s.buttons:
         b.check_mouse_over(x, y)
 
 @w.event
@@ -44,13 +45,17 @@ def on_mouse_press(x, y, button, modifiers):
             pass
         case None:
             pass
+    print(action)
 
 @w.event
 def on_resize(w, h):
     l.update(w, h)
 
 @w.event
-def on_key_press(s, m):
-    pass
+def on_key_press(sym, m):
+    if sym == 65453:
+        g.set_scale(g.scale - 0.5, w.width//2, w.height//2)
+    elif sym == 65451:
+        g.set_scale(g.scale + 0.5, w.width//2, w.height//2)
 
 pyglet.app.run()
